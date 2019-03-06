321efe9526e4b8af22de5af0a8fc24501a97b0cc
Eman Ashour <eman.ashour1111@gmail.com>
2019-03-06 09:55:10 -0600

    add Settings

--- app/src/main/java/com/example/timerkotlin/util/PrefUtil.kt
+++ app/src/main/java/com/example/timerkotlin/util/PrefUtil.kt
@@ -6,9 +6,10 @@ import com.example.timerkotlin.TimerActivity
 
 class PrefUtil {
     companion object {
+        private const val TIMER_LENGTH_ID = "timer_length"
         fun getTimerLength(context: Context): Int{
-            //placeholder
-            return 1
+            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+            return preferences.getInt(TIMER_LENGTH_ID, 10)
         }
 
         private const val PREVIOUS_TIMER_LENGTH_SECONDS_ID = "previous_timer_length_seconds"
